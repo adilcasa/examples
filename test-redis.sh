da66d6ea45e915b628bc3fbd2e0b3aba23f18209
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2017-11-27 11:41:52 +0100

    fix test

--- test-redis.sh
+++ test-redis.sh
@@ -3,7 +3,10 @@
 set -e
 set -x
 
-kubectl exec redis-2 -c redis   -- redis-cli -p 6379 sentinel get-master-addr-by-name redis
+# Shell
+# kubectl exec redis-2 -it -c redis   -- sh  
+
+kubectl exec redis-2 -c redis   -- redis-cli -p 26379 sentinel get-master-addr-by-name redis
 sleep 1
 kubectl exec redis-2 -c redis -- redis-cli -p 6379 get foo
 sleep 1
