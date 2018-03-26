9f30f55b1f1312dc6c897a3eb6b0c18bf0d0a4c7
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2018-03-26 17:37:36 +0200

    Improve solution scripts

--- check-all-commands.sh
+++ check-all-commands.sh
@@ -1,32 +1,42 @@
+set -e
+set -x
+
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+# Cleaning up
+"$DIR"/cleanup-default-ns.sh
+
 # Deploying a kubernetes cluster
 #
 kubectl get componentstatuses
 kubectl get nodes
 kubectl get deployments --namespace=kube-system kube-dns
 kubectl get service --namespace=kube-system kube-dns
-kubectl get svc --namespace=kube-system kube-dashboard
+kubectl get svc --namespace=kube-system kubernetes-dashboard
 kubectl get services --namespace=kube-system kubernetes-dashboard
 kubectl get deployments --namespace=kube-system kubernetes-dashboard
 
 # Common kubectl commands
 #
 # See https://kubernetes.io/docs/user-guide/kubectl-cheatsheet/
-kubectl run kuard --image=gcr.io/kuar-demo/kuard-adm64:1
-kubectl get pods kuard-xxxx -o jsonpath --template={.status.podIP}
+# kubectl run kuard --image=gcr.io/kuar-demo/kuard-adm64:1
+# kubectl get pods kuard-xxxx -o jsonpath --template={.status.podIP}
 # kubectl describe <resource-name> <obj-name>
+
 # Object management
-kubectl apply -f obj.yaml
-kubectl edit <resource-name> <obj-name>
-kubectl delete -f obj.yaml
-kubectl delete <resource-name> <obj-name>
+# kubectl apply -f obj.yaml
+# kubectl edit <resource-name> <obj-name>
+# kubectl delete -f obj.yaml
+# kubectl delete <resource-name> <obj-name>
+
 # Label
-kubectl label pods bar color=red
-kubectl label pods bar --overwrite color=green
-kubectl label pods bar -color
+# kubectl label pods bar color=red
+# kubectl label pods bar --overwrite color=green
+# kubectl label pods bar -color
 # Debug
-kubectl logs <pod-name>
-kubectl exec -it <podY-name> -- bash
-kubectl cp <pod-name>:/path/to/remote/file /path/to/local/file
+# kubectl logs <pod-name>
+# kubectl exec -it <podY-name> -- bash
+# kubectl cp <pod-name>:/path/to/remote/file /path/to/local/file
 
 # Pods
 #
@@ -36,9 +46,8 @@ kubectl delete deployments/kuard
 kubectl apply -f  5-1-kuard-pod.yaml
 kubectl get pods
 kubectl describe pods kuard
-kubectl delete pods/kuard
-kubectl delete -f  5-1-kuard-pod.yaml
 # Port forwarding host_port:pod-port
+sleep 5
 kubectl port-forward kuard 8081:8080
 # On host go to http://localhost:8081
 # Logs
@@ -55,6 +64,8 @@ docker exec -- kube-node-1 ls -rtla /var/lib/kuard
 # All together
 # Exercice: check if it works and eventually fix it
 kubectl apply -f  5-6-kuard-pod-full.yaml
+kubectl delete pods/kuard
+kubectl delete -f  5-1-kuard-pod.yaml
 
 # Label & Annotations
 #
